057db4241de25254b22097b87a24f58794a03761
Anders Inde <andersinde@gmail.com>
2019-07-07 10:13:49 +0200

    change some info

--- keyboards/handwired/planck/config.h
+++ keyboards/handwired/planck/config.h
@@ -1,19 +1,3 @@
-/*
-Copyright 2018 Brice Figureau <brice.figureau@gmail.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
 #ifndef CONFIG_H
 #define CONFIG_H
@@ -24,9 +8,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID 0xFEED
 #define PRODUCT_ID 0x6060
 #define DEVICE_VER 0x0001
-#define MANUFACTURER    masterzen
-#define PRODUCT         Planck
-#define DESCRIPTION     Handwired Planck
+#define MANUFACTURER    ainders
+#define PRODUCT         plink
+#define DESCRIPTION     handwired Planck
 
 /* key matrix size */
 #define MATRIX_ROWS 4
@@ -63,9 +47,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /* key combination for magic key command */
+/*
 #define IS_COMMAND() ( \
     keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
 )
-
+*/
 
 #endif
--- keyboards/handwired/planck/keymaps/ainders/keymap.c
+++ keyboards/handwired/planck/keymaps/ainders/keymap.c
@@ -55,14 +55,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift| Shift|  Z   |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Ctrl | GUI  | Alt  |Lower | Bksp |Space |Raise | Next | Vol- | Vol+ | Play |
+ * | Ctrl | Ctrl | Alt  | CMD  |Lower | Bksp |Space |Raise | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
     KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
     KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_ENT ,
-    KC_LCTL, KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_BSPC,  KC_SPC,  RAISE,  KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+    KC_LCTL, KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_BSPC, KC_SPC,  RAISE,  KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 [_DVORAK] = LAYOUT_planck_grid(
