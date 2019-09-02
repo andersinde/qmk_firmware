79437d2d240c4d1edd097e6f00c08fb8712ebeb7
Anders Inde <andersinde@gmail.com>
2019-09-02 17:16:10 +0200

    idk

--- keyboards/ainders/keymaps/colemak/keymap.c
+++ keyboards/ainders/keymaps/colemak/keymap.c
@@ -19,42 +19,58 @@ enum planck_keycodes {
   KC_O_UMLAUT  //ö
 };
 
-#define KC_LOWER LT(_LOWER, KC_TAB)
-#define KC_RAISE LT(_RAISE, KC_ENTER)
+#define KC_LOWER  LT(_LOWER, KC_TAB)
+#define KC_RAISE  LT(_RAISE, KC_ENTER)
+#define KC_CMD    CMD_T(KC_MUTE)
+#define KC_ALT    ALT_T(KC_MFFD)
 
-#define KC_NTAB LCTL(KC_TAB)  // switch tabs easily
-#define KC_PTAB LSFT(KC_NTAB)
+#define KC_NTAB   LCTL(KC_TAB)  // switch tabs easily
+#define KC_PTAB   LSFT(KC_NTAB)
 
 #define KC_SCRNSH LSFT(LCMD(KC_3))// screenshot on a mac
 #define KC_ESCTRL CTL_T(KC_ESC)  // tab when tapped, ctrl when held
-#define KC_SUPER LCAG_T(KC_NO)  // ctrl+alt+cmd when held
-#define KC_MSFT OSM(MOD_LSFT)  // sticky shift
+#define KC_SUPER  LCAG_T(KC_NO)  // ctrl+alt+cmd when held
+#define KC_MSFT   OSM(MOD_LSFT)  // sticky shift
 
-#define ONESHOT_TAP_TOGGLE 2
+#define ONESHOT_TAP_TOGGLE 2 // double tap to toggle
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+// [_COLEMAK] = LAYOUT_kc(
+//          ,  Q  ,  W  ,  F  ,  P  ,  G  ,        J  ,  L  ,  U  ,  Y  , SCLN,     ,
+//    ESCTRL,  A  ,  R  ,  S  ,  T  ,  D  ,        H  ,  N  ,  E  ,  I  ,  O  , QUOT,
+//     SUPER, MSFT,  Z  ,  X  ,  C  ,  V  ,        B  ,  K  ,  M  , COMM, DOT ,     ,
+//                    ,,, LCMD,LOWER, BSPC,       SPC ,RAISE, LALT,,,
+// ),
+//
+// [_COLEMAKDH] = LAYOUT_kc(
+//      TAB ,  Q  ,  W  ,  F  ,  P  ,  B  ,        J  ,  L  ,  U  ,  Y  , SCLN, BSPC,
+//    ESCTRL,  A  ,  R  ,  S  ,  T  ,  G  ,        M  ,  N  ,  E  ,  I  ,  O  , QUOT,
+//     SUPER,  Z  ,  X  ,  C  ,  D  ,  V  ,        K  ,  H  , COMM, DOT , SLSH, LALT,
+//                    ,,, LCMD,LOWER, MSFT,       SPC ,RAISE,     ,,,
+// ),
+
 [_COLEMAK] = LAYOUT_kc(
 // |-----+-----+-----+-----+-----+-----|     |-----+-----+-----+-----+-----+-----|
-     TAB ,  Q  ,  W  ,  F  ,  P  ,  B  ,        J  ,  L  ,  U  ,  Y  , SCLN, BSPC,
+         ,  X  ,  W  ,  F  ,  P  ,  Q  ,        J  ,  L  ,  U  ,  Y  ,  B  , SCLN,
 // |-----+-----+-----+-----+-----+-----|     |-----+-----+-----+-----+-----+-----|
    ESCTRL,  A  ,  R  ,  S  ,  T  ,  G  ,        M  ,  N  ,  E  ,  I  ,  O  , QUOT,
 // |-----+-----+-----+-----+-----+-----|     |-----+-----+-----+-----+-----+-----|
-    SUPER,  Z  ,  X  ,  C  ,  D  ,  V  ,        K  ,  H  , COMM, DOT , SLSH, LALT,
+    SUPER, MSFT,  Z  ,  C  ,  D  ,  V  ,        K  ,  H  , COMM, DOT , SLSH, SCLN,
 // |-----+-----+-----+-----+-----+-----|     |-----+-----+-----+-----+-----+-----|
-         ,     ,     , LCMD,LOWER, MSFT,       SPC ,RAISE, LEAD,     ,     ,
+                   ,,, CMD ,LOWER, BSPC,       SPC ,RAISE, ALT ,,,
 //                   |-----+-----+-----|     |-----+-----+-----|
 ),
 
 [_LOWER] = LAYOUT_kc(
 // |-----+-----+-----+-----+-----+-----|     |-----+-----+-----+-----+-----+-----|
-         ,COMM ,  1  ,  2  ,  3  ,     ,           , MINS, EQL , BSLS, TILD,     ,
+         , COMM,  1  ,  2  ,  3  , MINS,           , MINS, EQL , BSLS, TILD,     ,
 // |-----+-----+-----+-----+-----+-----|     |-----+-----+-----+-----+-----+-----|
-         ,  0  ,  4  ,  5  ,  6  ,     ,           , PTAB, NTAB, LBRC, RBRC,     ,
+         ,  0  ,  4  ,  5  ,  6  , PLUS,           , LPRN, RPRN, LBRC, RBRC,     ,
 // |-----+-----+-----+-----+-----+-----|     |-----+-----+-----+-----+-----+-----|
-         , DOT ,  7  ,  8  ,  9  ,     ,           , LPRN, RPRN, GRV ,     ,     ,
+         , DOT ,  7  ,  8  ,  9  , ASTR,           , PTAB, NTAB, GRV ,     ,     ,
 // |-----+-----+-----+-----+-----+-----|     |-----+-----+-----+-----+-----+-----|
-         ,     ,     ,     ,     ,     ,       LSFT,     ,     ,     , BRID, BRIU
+                   ,,,     ,     ,     ,       LSFT,     ,     ,,,
 //                   |-----+-----+-----|     |-----+-----+-----|
 ),
 
@@ -62,11 +78,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // |-----+-----+-----+-----+-----+-----|     |-----+-----+-----+-----+-----+-----|
          , VOLU,  F1 ,  F2 ,SCRNSH,    ,         ,A_CIRC,A_UMLAUT,O_UMLAUT,,     ,
 // |-----+-----+-----+-----+-----+-----|     |-----+-----+-----+-----+-----+-----|
-         , VOLD, MRWD, MPLY, MFFD,    ,       LEFT, DOWN,  UP , RGHT, COLN,     ,
+         , VOLD, MRWD, MPLY, MFFD,     ,       LEFT, DOWN,  UP , RGHT, COLN,     ,
 // |-----+-----+-----+-----+-----+-----|     |-----+-----+-----+-----+-----+-----|
          , MUTE,  F7 ,  F8 ,  F9 ,     ,           , PGDN, PGUP,     ,     ,     ,
 // |-----+-----+-----+-----+-----+-----|     |-----+-----+-----+-----+-----+-----|
-         ,     ,     ,     ,     , DEL,            ,     ,     ,     ,     ,
+                   ,,,     ,     , DEL ,           ,     ,     ,,,
 //                   |-----+-----+-----|     |-----+-----+-----|
 ),
 
@@ -78,7 +94,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // |-----+-----+-----+-----+-----+-----|     |-----+-----+-----+-----+-----+-----|
          ,     ,     ,     ,     ,     ,           ,     ,     ,     ,     ,     ,
 // |-----+-----+-----+-----+-----+-----|     |-----+-----+-----+-----+-----+-----|
-         ,     ,     ,     ,     ,     ,           ,     ,     ,     ,     ,
+//                   |-----+-----+-----|     |-----+-----+-----|
+                   ,,,     ,     ,     ,           ,     ,     ,,,
 //                   |-----+-----+-----|     |-----+-----+-----|
 )
 
@@ -90,16 +107,9 @@ uint32_t layer_state_set_user(uint32_t state) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case COLEMAK:
-      if (record->event.pressed) {
-        print("switched to colemak\n");
-        set_single_persistent_default_layer(_COLEMAK);
-      }
-      return false;
-      break;
     case KC_A_CIRC:
       if (record->event.pressed) {
-          SEND_STRING(SS_LALT("a"));
+        SEND_STRING(SS_LALT("a"));
       }
       return false;
       break;
